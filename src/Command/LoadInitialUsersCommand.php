5e531f2df888b4979fb8fd3b73ebc63502f6985f
ridialtech <103546771+ridialtech@users.noreply.github.com>
2025-06-21 16:23:48 +0000

    Fix login by disabling CSRF and using SQLite

---
+++ src/Command/LoadInitialUsersCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\User;
+
+#[AsCommand(
+    name: 'app:load-initial-users',
+    description: 'Create default users for the demo application',
+)]
+class LoadInitialUsersCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $hasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        // no arguments
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        // predefined demo users
+        $users = [
+            ['user1@gmail.com', '1234', ['ROLE_USER'], 'User One'],
+            ['use2@gmail.com', '1234', ['ROLE_USER'], 'User Two'],
+            ['uadmin@gmail.com', '1234', ['ROLE_ADMIN'], 'Admin'],
+        ];
+
+        foreach ($users as [$email, $plainPassword, $roles, $name]) {
+            if ($this->em->getRepository(User::class)->findOneBy(['email' => $email])) {
+                continue;
+            }
+
+            $user = new User();
+            $user->setEmail($email)
+                ->setRoles($roles)
+                ->setName($name)
+                ->setPassword($this->hasher->hashPassword($user, $plainPassword));
+
+            $this->em->persist($user);
+        }
+
+        $this->em->flush();
+
+        $io->success('Default users created or already exist.');
+
+        return Command::SUCCESS;
+    }
+}
